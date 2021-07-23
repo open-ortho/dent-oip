3c859953b09d64a680c9264a3e19b5fae7c7780e
Toni Magni <amagni@topsortho.com>
2021-07-23 14:43:52 -0400

    Add docxbuilder

--- source/conf.py
+++ source/conf.py
@@ -20,6 +20,7 @@
 project = 'ADA-1107'
 copyright = '2021, ADA SCDI WG 11.6'
 author = 'ADA SCDI WG 11.6'
+master_doc = 'index'
 
 # The full version, including alpha/beta/rc tags
 release = '0.1'
@@ -30,7 +31,7 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = [ 'docxbuilder'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -54,3 +55,16 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- Options for docx output -------------------------------------------------
+
+# docx_documents = [
+#     ('index', 'ADA-1107.docx', {
+#          'title': project,
+#          'created': author
+#          'subject': 'Sphinx builder extension',
+#          'keywords': ['Interoperability', 'Orthodontics']
+#      }, True),
+# ]
+# docx_style = 'path/to/custom_style.docx'
+docx_pagebreak_before_section = 2
