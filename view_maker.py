f715cd8be2b7516b056c1f382e9bbf5cbbb4430a
Toni Magni <afm@case.edu>
2022-06-01 14:33:06 +0200

    Autoformat.

--- view_maker.py
+++ view_maker.py
@@ -6,23 +6,23 @@ import sqlite3
 import csv
 
 # Prefixes
-PREFIX_SNOMED='SCT '
-PREFIX_DICOM='DCM '
+PREFIX_SNOMED = "SCT "
+PREFIX_DICOM = "DCM "
 
 # Files and paths
-PATH_TABLES = os.path.join('.','source','tables')
-PATH_TABLES_GENERATED = os.path.join(PATH_TABLES, 'generated')
-CSV_SNOMED = os.path.join(PATH_TABLES,'codes_snomed.csv')
-CSV_DICOM = os.path.join(PATH_TABLES,'codes_dicom.csv')
-CSV_DICOM_TAGS = os.path.join(PATH_TABLES,'tags_dicom.csv')
-CSV_VIEWS = os.path.join(PATH_TABLES,'views.csv')
-DBFILE='views.db'
+PATH_TABLES = os.path.join(".", "source", "tables")
+PATH_TABLES_GENERATED = os.path.join(PATH_TABLES, "generated")
+CSV_SNOMED = os.path.join(PATH_TABLES, "codes_snomed.csv")
+CSV_DICOM = os.path.join(PATH_TABLES, "codes_dicom.csv")
+CSV_DICOM_TAGS = os.path.join(PATH_TABLES, "tags_dicom.csv")
+CSV_VIEWS = os.path.join(PATH_TABLES, "views.csv")
+DBFILE = "views.db"
 
 # Database Tables
-T_VIEWS='ortho_views'
-T_SNOMED='codes_snomed'
-T_DICOM='codes_dicom'
-T_DICOM_TAGS='tags_dicom'
+T_VIEWS = "ortho_views"
+T_SNOMED = "codes_snomed"
+T_DICOM = "codes_dicom"
+T_DICOM_TAGS = "tags_dicom"
 
 # Database Columns
 C_VIE = "view_name"
@@ -40,7 +40,7 @@ C_OCR = "occlusal_relationship"
 con = sqlite3.connect(DBFILE)
 cur = None
 
-IV_RST = '''
+IV_RST = """
 {IV_TITLE}
 
 .. figure:: {IV_IMAGE_FILENAME}
@@ -60,13 +60,16 @@ Primary Anatomic Structure Sequence
 See section :ref:`primary anatomic structure sequence`
 
 {IV_EXAMPLE}
-'''
+"""
 
-def iv_print_rst(title,filename,number):
-    print(IV_RST.format(IV_TITLE=title,IV_IMAGE_FILENAME=filename,IV_NUMBER=number))
+
+def iv_print_rst(title, filename, number):
+    print(IV_RST.format(IV_TITLE=title, IV_IMAGE_FILENAME=filename, IV_NUMBER=number))
+
 
 def initdb(cur):
-    cur.execute(f'''
+    cur.execute(
+        f"""
     CREATE TABLE {T_VIEWS}(
         {C_VIE} text,
         {C_POR} text,
@@ -79,71 +82,92 @@ def initdb(cur):
         {C_IMV} text,
         {C_FCA} text,
         {C_OCR} text)
-        ''')
+        """
+    )
 
-    cur.execute(f'''
+    cur.execute(
+        f"""
     CREATE TABLE {T_SNOMED}(
         id text,
         code int,
         meaning text)
-        ''')
+        """
+    )
     load_snomed_codes(cur)
 
-    cur.execute(f'''
+    cur.execute(
+        f"""
     CREATE TABLE {T_DICOM}(
         id text,
         code int,
         meaning text)
-        ''')
+        """
+    )
     load_dicom_codes(cur)
 
-    cur.execute(f'''
+    cur.execute(
+        f"""
     CREATE TABLE {T_DICOM_TAGS}(
         id text,
         attribute_name text,
         tag text)
-        ''')
+        """
+    )
     load_dicom_tags(cur)
 
+
 def load_snomed_codes(cur):
-    with open(CSV_SNOMED,'r') as fin:
+    with open(CSV_SNOMED, "r") as fin:
         # csv.DictReader uses first line in file for column headings by default
-        dr = csv.DictReader(fin) # comma is default delimiter
-        to_db = [(i['id'],i['code'], i['meaning']) for i in dr]
-    cur.executemany(f"INSERT INTO {T_SNOMED} (id, code, meaning) VALUES (?, ?, ?);", to_db)
+        dr = csv.DictReader(fin)  # comma is default delimiter
+        to_db = [(i["id"], i["code"], i["meaning"]) for i in dr]
+    cur.executemany(
+        f"INSERT INTO {T_SNOMED} (id, code, meaning) VALUES (?, ?, ?);", to_db
+    )
+
 
 def load_dicom_codes(cur):
-    with open(CSV_DICOM,'r') as fin:
+    with open(CSV_DICOM, "r") as fin:
         # csv.DictReader uses first line in file for column headings by default
-        dr = csv.DictReader(fin) # comma is default delimiter
-        to_db = [(i['id'],i['code'], i['meaning']) for i in dr]
-    cur.executemany(f"INSERT INTO {T_DICOM} (id, code, meaning) VALUES (?, ?, ?);", to_db)
+        dr = csv.DictReader(fin)  # comma is default delimiter
+        to_db = [(i["id"], i["code"], i["meaning"]) for i in dr]
+    cur.executemany(
+        f"INSERT INTO {T_DICOM} (id, code, meaning) VALUES (?, ?, ?);", to_db
+    )
+
 
 def load_dicom_tags(cur):
-    with open(CSV_DICOM_TAGS,'r') as fin:
+    with open(CSV_DICOM_TAGS, "r") as fin:
         # csv.DictReader uses first line in file for column headings by default
-        dr = csv.DictReader(fin) # comma is default delimiter
-        to_db = [(i['id'],i['attribute_name'], i['tag']) for i in dr]
-    cur.executemany(f"INSERT INTO {T_DICOM_TAGS} (id, attribute_name, tag) VALUES (?, ?, ?);", to_db)
+        dr = csv.DictReader(fin)  # comma is default delimiter
+        to_db = [(i["id"], i["attribute_name"], i["tag"]) for i in dr]
+    cur.executemany(
+        f"INSERT INTO {T_DICOM_TAGS} (id, attribute_name, tag) VALUES (?, ?, ?);", to_db
+    )
+
 
 def load_views(cur):
-    with open(CSV_VIEWS,'r') as fin:
+    with open(CSV_VIEWS, "r") as fin:
         # csv.DictReader uses first line in file for column headings by default
-        dr = csv.DictReader(fin) # comma is default delimiter
-        to_db = [(
-            i[C_VIE],
-            i[C_POR],
-            i[C_LAT],
-            i[C_ARS],
-            i[C_ARM],
-            i[C_PAM],
-            i[C_DEV],
-            i[C_AQV],
-            i[C_IMV],
-            i[C_FCA],
-            i[C_OCR]
-            ) for i in dr]
-    cur.executemany(f'''
+        dr = csv.DictReader(fin)  # comma is default delimiter
+        to_db = [
+            (
+                i[C_VIE],
+                i[C_POR],
+                i[C_LAT],
+                i[C_ARS],
+                i[C_ARM],
+                i[C_PAM],
+                i[C_DEV],
+                i[C_AQV],
+                i[C_IMV],
+                i[C_FCA],
+                i[C_OCR],
+            )
+            for i in dr
+        ]
+    cur.executemany(
+        f"""
     INSERT INTO {T_VIEWS} (
         {C_VIE},
         {C_POR},
@@ -156,15 +180,19 @@ def load_views(cur):
         {C_IMV},
         {C_FCA},
         {C_OCR})
-        VALUES (?,?,?,?,?,?,?,?,?,?,?);''', to_db)
+        VALUES (?,?,?,?,?,?,?,?,?,?,?);""",
+        to_db,
+    )
+
 
 def close_connection():
     con.commit()
     con.close()
 
+
 def create_view(cur, view_type):
 
-    query_view = f'''
+    query_view = f"""
 -- SQLite
 CREATE TEMP TABLE IF NOT EXISTS _temp (
     id text,
@@ -299,12 +327,12 @@ FROM (
         WHERE ortho_views.view_name LIKE '{view_type}'
     ) as {C_OCR}
 WHERE id = '{C_OCR}';
-'''
+"""
     try:
         os.makedirs(PATH_TABLES_GENERATED)
     except FileExistsError:
         pass
-    output_file = os.path.join(PATH_TABLES_GENERATED,view_type + '.csv')
+    output_file = os.path.join(PATH_TABLES_GENERATED, view_type + ".csv")
     try:
         cur.executescript(query_view)
     except sqlite3.OperationalError as e:
@@ -312,14 +340,16 @@ WHERE id = '{C_OCR}';
         print(query_view)
         exit(1)
 
-    cur.execute('''SELECT
+    cur.execute(
+        """SELECT
         attribute_name AS "Attribute Name",
         tag AS 'Tag',
         code_value AS 'Value',
         meaning AS 'Meaning'
     FROM _temp;
-    ''')
-    with open(output_file,'w') as out_csv_file:
+    """
+    )
+    with open(output_file, "w") as out_csv_file:
         print(f"Writing to file {output_file}")
         csv_out = csv.writer(out_csv_file)
         # write header
@@ -328,6 +358,7 @@ WHERE id = '{C_OCR}';
         for result in cur:
             csv_out.writerow(result)
     cur.execute("DELETE FROM _temp")
+
 
 def main(args):
     print("Main")
@@ -338,12 +369,10 @@ def main(args):
     cur2 = con.cursor()
     for view in cur2.execute(f"SELECT view_name FROM {T_VIEWS}"):
         create_view(cur, view[0])
-        iv_print_rst(
-            title='',
-        filename = f'../images/{view[0]}.png',
-        number = view[0])
+        iv_print_rst(title="", filename=f"../images/{view[0]}.png", number=view[0])
 
     close_connection()
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
