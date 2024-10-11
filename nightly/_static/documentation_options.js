f054793905a51b3c12170bf5ed715bfc10740b87
zgypa <zgypa@users.noreply.github.com>
2024-10-11 01:25:13 +0000

    deploy: 538b69ba4ba32a183fec526d7aa2f954db234ba3

--- nightly/_static/documentation_options.js
+++ nightly/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.2.7-nightly.20241010012433',
+    VERSION: '0.2.7-nightly.20241011012426',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
