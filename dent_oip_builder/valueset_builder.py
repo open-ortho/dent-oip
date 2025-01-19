b56f52b946d11838e15cfb7e83dc65a051b79375
Toni Magni <email@antoniomagni.com>
2025-01-20 00:14:01 +0100

    Correct V3_Appendix structure

--- dent_oip_builder/valueset_builder.py
+++ dent_oip_builder/valueset_builder.py
@@ -4,6 +4,8 @@ The various tables which rely on various codes which can be obtaines as FHIR Val
 
 """
 import requests
+import csv
+from pathlib import Path
 
 urls = [
     "http://terminology.open-ortho.org/fhir/extraoral-2d-photographic-scheduled-protocol",
@@ -12,12 +14,14 @@ urls = [
     "http://terminology.open-ortho.org/fhir/intraoral-3d-visible-light-scheduled-protocol"
 ]
 
+
 def download_fhir_codesystem_to_rst(url, output_file):
     response = requests.get(url)
     response.raise_for_status()
     codesystem = response.json()
 
     with open(output_file, 'a') as file:
+        file.write(f".. _{codesystem['id']}:\n")
         file.write(f".. list-table:: {codesystem['title']}\n")
         file.write("    :header-rows: 1\n\n")
         file.write("    * - code scheme designator\n")
@@ -31,12 +35,34 @@ def download_fhir_codesystem_to_rst(url, output_file):
             file.write(f"      - {concept['display']}\n")
             file.write("      - \n")
 
-def main():
 
+def download_fhir_codesystem_to_csv(url, output_file):
+    response = requests.get(url)
+    response.raise_for_status()
+    codesystem = response.json()
+
+    with open(output_file, 'a', newline='', encoding='utf-8') as file:
+        # get code scheme designator from codesystem
+        if 'identifier' in codesystem:
+            codeschemedesignator = codesystem['identifier'][0]['value']
+        else:
+            codeschemedesignator = codesystem['url']
+        writer = csv.writer(file)
+        writer.writerow(["Attribute Name", "Tag", "Value"])
+        for concept in codesystem['concept']:
+            writer.writerow(
+                [codeschemedesignator, concept['code'], concept['display']])
+
+
+def main():
     for url in urls:
-        output_file = url.split('/')[-1] + '.rst'
-        download_fhir_codesystem_to_rst(url, output_file)
+        output_base_path = Path('source') / 'tables' / 'generated'
+        # Create the directory if it doesn't exist
+        output_base_path.mkdir(parents=True, exist_ok=True)
+        for url in urls:
+            output_file = output_base_path / (url.split('/')[-1] + '.csv')
+            download_fhir_codesystem_to_csv(url, output_file)
+
 
 if __name__ == "__main__":
     main()
-
