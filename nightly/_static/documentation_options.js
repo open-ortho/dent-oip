b06d287416526b7d5642cfd61ddbb60debb169b1
felhoferl <felhoferl@users.noreply.github.com>
2026-02-11 16:57:39 +0000

    deploy: c3bcb8706b080f970492d0aef1f49527b23d48f0

--- nightly/_static/documentation_options.js
+++ nightly/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.2.7-nightly.20260211164212',
+    VERSION: '0.2.7-nightly.20260211165645',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
